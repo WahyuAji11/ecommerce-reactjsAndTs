3d9df9f6b2803f45ff921b8652c423f61eed9ea0
WahyuAji11 <alhafidzw1@gmail.com>
2024-08-10 13:35:05 +0800

    repair product images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['firebasestorage.googleapis.com', 'th.bing.com', 'lh3.googleusercontent.com']
+        domains: ['firebasestorage.googleapis.com', 'th.bing.com', 'lh3.googleusercontent.com', 'www.bhphotovideo.com']
     },
 }
 
--- utils/products.tsx
+++ utils/products.tsx
@@ -1,28 +1,28 @@
 export const products = [
-{
-    id: "64a654593e91b8e73a351e9b",
-    name: "iphone 14",
-    description: "Short description",
-    price: 2999,
-    brand: "apple",
-    category: "Phone",
-    inStock: true,
-    images: [
-    {
-        color: "White",
-        colorCode: "#FFFFFF",
-        image:
-        "https://firebasestorage.googleapis.com/v0/b/e-shop-vid.appspot.com/o/products%2F1694245643713-iphone14-white.png?alt=media&token=2486cd23-8aa9-449e-8563-8c8a3278d0b4",
-    },
-    {
-        color: "Gray",
-        colorCode: "#808080",
-        image:
-        "https://firebasestorage.googleapis.com/v0/b/e-shop-vid.appspot.com/o/products%2F1694245647731-iphone14-gray.png?alt=media&token=ba0019e0-a6cb-4da7-b214-6252bf57f7e3",
-    },
-    ],
-    reviews: [],
-},
+// {
+//     id: "64a654593e91b8e73a351e9b",
+//     name: "iphone 14",
+//     description: "Short description",
+//     price: 2999,
+//     brand: "apple",
+//     category: "Phone",
+//     inStock: true,
+//     images: [
+//     {
+//         color: "White",
+//         colorCode: "#FFFFFF",
+//         image:
+//         "https://firebasestorage.googleapis.com/v0/b/e-shop-vid.appspot.com/o/products%2F1694245643713-iphone14-white.png?alt=media&token=2486cd23-8aa9-449e-8563-8c8a3278d0b4",
+//     },
+//     {
+//         color: "Gray",
+//         colorCode: "#808080",
+//         image:
+//         "https://firebasestorage.googleapis.com/v0/b/e-shop-vid.appspot.com/o/products%2F1694245647731-iphone14-gray.png?alt=media&token=ba0019e0-a6cb-4da7-b214-6252bf57f7e3",
+//     },
+//     ],
+//     reviews: [],
+// },
 {
     id: "64a4ebe300900d44bb50628a",
     name: "Logitech MX Keys Advanced Wireless Illuminated Keyboard, Tactile Responsive Typing, Backlighting, Bluetooth, USB-C, Apple macOS, Microsoft Windows, Linux, iOS, Android, Metal Build (Black)",
@@ -37,7 +37,7 @@ export const products = [
         color: "Black",
         colorCode: "#000000",
         image:
-        "https://th.bing.com/th/id/OIP.I-bdk5ct7HMyJ567YnAwWwHaHa?w=1000&h=1000&rs=1&pid=ImgDetMain",
+        "https://www.bhphotovideo.com/images/images500x500/logitech_920_009552_mx_keys_wireless_keyboard_1637662834_1571629.jpg",
     },
     ],
     reviews: [
